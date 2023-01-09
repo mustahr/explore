377959fc9edc5b7198ad013e64a841203bbc15da
musta hr <mustaphaharmouch1972@gmail.com>
2023-01-09 12:15:45 +0100

    remove

--- files/conf/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect("localhost", "u333140862_explore", "Mustaphaharmouch@2000", "u333140862_explore");
-error_reporting(0);
-
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-}
